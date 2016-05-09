3f4bee8f21db038c48687d2fac8869a46726499d
Denis Svidunovich <volverinemail@gmail.com>
2016-05-09 22:48:00 +0300

    Check, reversed, key are working fine. Bugs with usual run fixed.

--- ISP/4_term/Lab2/task1.py
+++ ISP/4_term/Lab2/task1.py
@@ -3,12 +3,87 @@ import tempfile
 import os
 
 
-def cmp1(first, second):
-    return first < second
+def strNumsToIntList(str):
+    ans = []
+    cur = []
+
+    for i in str:
+        if i == ',':
+            ans.append(''.join(cur))
+            del cur[:]
+
+        cur.append(i)
+
+    if len(cur) > 0:
+        ans.append(''.join(cur))
+        del cur[:]
+
+    ans = map(int, ans)
+    
+    return ans
 
 
-def cmp2(first, second):
-    return second < first
+
+def cmp1(tfirst, tsecond, key, numeric):
+    first = []
+    second = []
+
+    if type(key) == str and key == 'all':
+        first = tfirst
+        second = tsecond
+    else:
+        for i in key:
+            first.append(tfirst[i])
+            second.append(tsecond[i])
+
+    if numeric:
+        first = map(int, first)
+        second = map(int, second)
+
+        for i in xrange(min(len(first), len(second))):
+            if first[i] > second[i]:
+                return False
+
+        if len(first) <= len(second):
+            return True
+        else:
+            return False
+    else:
+        first = ''.join(first)
+        second = ''.join(second)
+        return first <= second
+
+
+def cmp2(tfirst, tsecond, numeric):
+    first = []
+    second = []
+
+    if type(key) == str and key == 'all':
+        first = tfirst
+        second = tsecond
+
+
+    for i in key:
+        first.append(tfirst[i])
+        second.append(tsecond[i])
+
+
+    if numeric:
+        first = map(int, first)
+        second = map(int, second)
+
+        for i in xrange(min(len(first), len(second))):
+            if first[i] < second[i]:
+                return False
+
+        if len(first) >= len(second):
+            return True
+        else:
+            return False
+    else:
+        first = ''.join(first)
+        second = ''.join(second)
+        return first >= second
 
 
 def read_line(f, fields_separator, lines_separator):
@@ -34,9 +109,11 @@ def read_line(f, fields_separator, lines_separator):
     return fields
 
 
-def sort(cmp, in_file_name=None, out_file_name=None, lines_separator=None,
+def check(cmp=None, in_file_name=None, out_file_name=None, lines_separator=None,
             fields_separator=None, key=None, numeric=None, bufer_size=None):
 
+    if cmp == None:
+        cmp = cmp1
     if in_file_name == None:
         in_file_name = "input"
     if out_file_name == None:
@@ -47,6 +124,46 @@ def sort(cmp, in_file_name=None, out_file_name=None, lines_separator=None,
         fields_separator = '\t'
     if key == None:
         key = 'all'
+    elif type(key) == str and key != 'all':
+        key = strNumsToIntList(key)
+    if numeric == None:
+        numeric = False
+    if bufer_size == None:
+        bufer_size = 1024
+
+    with open(in_file_name, 'r') as f:
+        prev_line = read_line(f, fields_separator, lines_separator)
+        
+        while 42:
+            next_line = read_line(f, fields_separator, lines_separator)
+            if len(next_line) == 0:
+                break
+
+            if not cmp(prev_line, next_line, key, numeric):
+                return False
+
+    return True
+
+
+
+
+def sort(cmp=None, in_file_name=None, out_file_name=None, lines_separator=None,
+            fields_separator=None, key=None, numeric=None, bufer_size=None):
+
+    if cmp == None:
+        cmp = cmp1
+    if in_file_name == None:
+        in_file_name = "input"
+    if out_file_name == None:
+        out_file_name = "output"
+    if lines_separator == None:
+        lines_separator = '\n'    
+    if fields_separator == None:
+        fields_separator = '\t'
+    if key == None:
+        key = 'all'
+    elif type(key) == str and key != 'all':
+        key = strNumsToIntList(key)
     if numeric == None:
         numeric = False
     if bufer_size == None:
@@ -67,21 +184,25 @@ def main():
     parser.add_argument("-k", "--key", action="store", default="all", help="Defines indexes in string to sort.")
     parser.add_argument("-n", "--numeric", action="store_true", help="Defines to interpret fields as numbers.")
     parser.add_argument("--check", action="store_true", help="Defines only to check if sequence is sorted according to all setted options.")
-    parser.add_argument("--bufer_size", action="store", default="1024", help="Defines sort buffer size.")
+    parser.add_argument("--buffer_size", action="store", default="1024", help="Defines sort buffer size.")
     parser.add_argument("--reverse", action="store_true", help="Defines if sort should be reversed.")
 
     args = parser.parse_args()
+    
+    if args.reverse:
+        cmp = cmp2
+    else:
+        cmp = cmp1
 
     if args.check:
-        print check(args)
-    elif args.reversed:
-        sort(args, cmp1)
+        print check(cmp, args.input, args.output, args.lines_separator,
+                args.fields_separator, args.key, args.numeric, args.buffer_size)
     else:
-        sort(args, cmp2)
+        sort(args, cmp)
 
 
 
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
