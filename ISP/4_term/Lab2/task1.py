9a351a4423a9c8504b7f138eaec31cbf5cbcadd9
Denis Svidunovich <volverinemail@gmail.com>
2016-05-11 00:18:45 +0300

    Sort alpha version.

--- ISP/4_term/Lab2/task1.py
+++ ISP/4_term/Lab2/task1.py
@@ -41,6 +41,8 @@ def cmp1(tfirst, tsecond, key, numeric):
         for i in xrange(min(len(first), len(second))):
             if first[i] > second[i]:
                 return False
+            elif first[i] < second[i]:
+                return True
 
         if len(first) <= len(second):
             return True
@@ -70,6 +72,8 @@ def cmp2(tfirst, tsecond, key, numeric):
         for i in xrange(min(len(first), len(second))):
             if first[i] < second[i]:
                 return False
+            elif first[i] > second[i]:
+                return True
 
         if len(first) >= len(second):
             return True
@@ -88,6 +92,7 @@ def merge_sort(arr, cmp, key, numeric):
 
         left = sort(arr[:len(arr) / 2])
         right = sort(arr[len(arr) / 2:])
+
         
         del arr[:]
         l = 0
@@ -108,6 +113,7 @@ def merge_sort(arr, cmp, key, numeric):
             arr.append(right[r])
             r += 1
 
+        # print "{} will be merged with {} \n resul = {}\n".format(left, right, arr)
         return arr
     
     arr = sort(arr)
@@ -196,18 +202,21 @@ def sort(cmp=None, in_file_name=None, out_file_name=None, lines_separator=None,
     if buff_size == None:
         buff_size = 1048576
 
+    
+    def writeLineToFile(f, line):
+        f.write(str(line[0]))
+
+        for j in xrange(1, len(line)):
+            f.write(fields_separator)
+            f.write(str(line[j]))
+
+        f.write(lines_separator)
+
 
     def writeToFile(f, lines):
         #print "to file {0}, write {1}".format(f, lines)
         for line in lines:
-            f.write(line[0])
-
-            for j in xrange(1, len(line)):
-                f.write(fields_separator)
-                f.write(line[j])
-
-            f.write(lines_separator)
-
+            writeLineToFile(f, line) 
         f.seek(0)
 
 
@@ -221,7 +230,7 @@ def sort(cmp=None, in_file_name=None, out_file_name=None, lines_separator=None,
 
             while 42:
                 line = read_line(f, fields_separator, lines_separator, numeric)
-                print line
+
                 if line == []:
                     break
                 if cur_buffsize + sys.getsizeof(line) >= buff_size:
@@ -244,30 +253,79 @@ def sort(cmp=None, in_file_name=None, out_file_name=None, lines_separator=None,
                     temp_files.append(tmp)
                     del lines[:]
 
-            print temp_files
-            print cur_buffsize
 
-            for i in xrange(8):
-                print read_line(temp_files[0], fields_separator, lines_separator, numeric)
+    def mergeTwoFiles(file1, file2):
+        tmp = tempfile.TemporaryFile()
+
+        f1 = read_line(file1, fields_separator, lines_separator, numeric)
+        f2 = read_line(file2, fields_separator, lines_separator, numeric)
+
+        while f1 != [] and f2 != []:
+            if cmp(f1, f2, key, numeric):
+                writeLineToFile(tmp, f1)
+                f1 = read_line(file1, fields_separator, lines_separator, numeric)
+            else:
+                writeLineToFile(tmp, f2)
+                f2 = read_line(file2, fields_separator, lines_separator, numeric)
+
+        while f1 != []:
+            writeLineToFile(tmp, f1)
+            f1 = read_line(file1, fields_separator, lines_separator, numeric)
+
+        while f2 != []:
+            writeLineToFile(tmp, f2)
+            f2 = read_line(file2, fields_separator, lines_separator, numeric)
+
+        file1.close()
+        file2.close()
+        tmp.seek(0)
+
+        return tmp
+
+
+    def merge_sortedTempFiles():
+
+        current_tempfiles = []
+        while 42:
+            if len(temp_files) == 1:
+                break
+            if len(temp_files) % 2 != 0:
+                current_tempfiles.append(temp_files[-1])
+                del temp_files[-1]
+
+            for i in xrange(0, len(temp_files), 2):
+                merged = mergeTwoFiles(temp_files[i], temp_files[i + 1])
+                current_tempfiles.append(merged)
+
+            # temp_files = current_tempfiles[:]           DON'T WORKS WHY??????
+
+            del temp_files[:]
+            for i in current_tempfiles:
+                temp_files.append(i)
+            del current_tempfiles[:]
 
 
     to_sortedTempFiles()
+    merge_sortedTempFiles()
 
-#    def merge_sort():
+    with open(out_file_name, "w") as f:
+        line = read_line(temp_files[0], fields_separator, lines_separator, numeric)
 
-#    merge_sort()
-    return
+        while line != []:
+            writeLineToFile(f, line)
+            line = read_line(temp_files[0], fields_separator, lines_separator, numeric)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Scpecify huge sort.")
     parser.add_argument("-i", "--input", action="store", default="input", help="Defines input file.")
     parser.add_argument("-o", "--output", action="store", default="output", help = "Defines output file.")
     parser.add_argument("-ls", "--lines_separator", action="store", default="\n", help="Defines separator of lines in file.")
-    parser.add_argument("-fs", "--fields_separator", action="store", default="\n", help="Defines separator of fields in line.")
+    parser.add_argument("-fs", "--fields_separator", action="store", default="\t", help="Defines separator of fields in line.")
     parser.add_argument("-k", "--key", action="store", default="all", help="Defines indexes in string to sort.")
     parser.add_argument("-n", "--numeric", action="store_true", help="Defines to interpret fields as numbers.")
     parser.add_argument("--check", action="store_true", help="Defines only to check if sequence is sorted according to all setted options.")
-    parser.add_argument("--buffer_size", action="store", default="1048576", help="Defines sort buffer size.")
+    parser.add_argument("--buffer_size", action="store", default="10241024", help="Defines sort buffer size.")
     parser.add_argument("--reverse", action="store_true", help="Defines if sort should be reversed.")
 
     args = parser.parse_args()
