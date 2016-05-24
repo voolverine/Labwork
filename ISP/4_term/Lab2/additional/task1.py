a8fa50eb965b12d5da45da7c138148d9e54a1bf9
Denis Svidunovich <volverinemail@gmail.com>
2016-05-24 23:01:36 +0300

    From json version 0.2; TODO: fix problem with ':' in dict.

--- ISP/4_term/Lab2/additional/task1.py
+++ ISP/4_term/Lab2/additional/task1.py
@@ -11,24 +11,85 @@ def remove_indentation(text):
     return "".join(result)
 
 
+def split_by_comma(line):
+    result = []
+    temp = []
+
+    curly_brackets_count = 0
+    square_brackets_count = 0
+    quotes_count = 0
+
+    for character in line:
+        if character == '\"':
+            quotes_count ^= 1
+        elif character == '{' and quotes_count == 0:
+            curly_brackets_count += 1
+        elif character == '[' and quotes_count == 0:
+            square_brackets_count += 1
+        elif character == '}' and quotes_count == 0:
+            curly_brackets_count -= 1
+        elif character == ']' and quotes_count == 0:
+            square_brackets_count -= 1
+
+        if character == ',' and \
+                curly_brackets_count == 0 and \
+                square_brackets_count == 0 and \
+                quotes_count == 0:
+            
+            result.append(''.join(temp))
+            temp = []
+
+        else:
+            temp.append(character)
+
+    if len(temp) != 0:
+        result.append(''.join(temp))
+
+    return result
+
+
 def from_json(text):
     line = remove_indentation(text) 
+    print line
     
-    isDict =      re.compile('^\{[.]+\}$')
-    isList =      re.compile('^\[[.]+\]$')
+    isDict =      re.compile('^\{.+\}$')
+    isList =      re.compile('^\[.+\]$')
     isInt =       re.compile('^-?[0-9]+$')
-    isFloat =     re.compile('^-?([0-9]+)+(.[0-9]*)?$')
-    isBoolTrue =  re.compile('^True$')
-    isBoolFalse = re.compile('^False$')
+    isFloat =     re.compile('^-?([0-9]+)+(,[0-9]*)?$')
+    isBoolTrue =  re.compile('^true$')
+    isBoolFalse = re.compile('^false$')
     isString =    re.compile('^\"[^\']+\"$')
-    isPair =      re.compile('(?P<key>.+):(?P<value>.+)')
+    isPair =      re.compile('(?P<key>[^:]+):(?P<value>.+)')
 
     if isDict.match(line):
-        inner = line[1:-1].split(',')
+        inner = split_by_comma(line[1:-1])
 
         result = dict()
         for i in inner:
             pair = isPair.match(i)
+            result[from_json(pair.group('key'))] = from_json(pair.group('value'))
+
+        return result
+    elif isList.match(line):
+        inner = split_by_comma(line[1:-1])
+
+        result = list()
+        for i in inner:
+            result.append(from_json(i))
+
+        return result
+    elif isInt.match(line):
+        return int(line)
+    elif isFloat.match(line):
+        return float(line)
+    elif isBoolTrue.match(line):
+        return True
+    elif isBoolFalse.match(line):
+        return False
+    elif isString.match(line):
+        return line[1:-1]
+    else:
+        raise TypeError("Not json valid string.")
 
 
 
