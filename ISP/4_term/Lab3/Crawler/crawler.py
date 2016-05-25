b47ddd5c3f32a25530be7d18df952157728598ba
Denis Svidunovich <volverinemail@gmail.com>
2016-05-26 00:26:35 +0300

    Crawler downloading started.

--- ISP/4_term/Lab3/Crawler/crawler.py
+++ ISP/4_term/Lab3/Crawler/crawler.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import urllib2
 
-class Crawler(Object):
-    pass
+
+
+class Crawler(object):
+
+    def downloadUrl(url):
+        user_agent = "Simple python crawler"
+        headers = {'User-Agent': user_agent}
+
+        data = {} 
+
+        req = urllib2.Request(url, data, headers)
+        response = urllib2.urlopen(req)
+        
+        if response.code >= 400:
+            raise Exception("Error code {} while downloading {}".format(response.code, url))
 
 
 def main():
