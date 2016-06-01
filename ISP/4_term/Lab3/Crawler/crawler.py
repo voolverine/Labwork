fd3c486fdc2855e58cfd9902d659f4907d510ecd
Denis Svidunovich <volverinemail@gmail.com>
2016-06-01 13:15:14 +0300

    Url downloading perfectly. All links getting good.

--- ISP/4_term/Lab3/Crawler/crawler.py
+++ ISP/4_term/Lab3/Crawler/crawler.py
@@ -1,26 +1,34 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
-
+from bs4 import BeautifulSoup
 
 
 class Crawler(object):
 
+    @staticmethod
     def downloadUrl(url):
         user_agent = "Simple python crawler"
-        headers = {'User-Agent': user_agent}
 
-        data = {} 
-
-        req = urllib2.Request(url, data, headers)
+        req = urllib2.Request(url)
+        req.add_header('User-Agent', user_agent)
         response = urllib2.urlopen(req)
         
+        print response.code
         if response.code >= 400:
             raise Exception("Error code {} while downloading {}".format(response.code, url))
+        
+        return response.read()
 
+    @staticmethod
+    def get_all_urls_from_page(page):
+        soup = BeautifulSoup(page)
+        for a in soup.find_all('a', href=True):
+            print "Found the URL: ", a['href']
 
 def main():
-    pass 
+    page = Crawler.downloadUrl("http://docs.python.org/")
+    print Crawler.get_all_urls_from_page(page)
 
 
 if __name__ == "__main__":
