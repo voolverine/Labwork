1e8f7085af59828202979d856f17848210ad98b1
Denis Svidunovich <volverinemail@gmail.com>
2016-06-03 11:18:36 +0300

    Threading added.

--- ISP/4_term/Lab3/Crawler/crawler.py
+++ ISP/4_term/Lab3/Crawler/crawler.py
@@ -1,11 +1,53 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
+import threading
+import Queue
+import time
 from bs4 import BeautifulSoup
 from urlparse import urljoin
 
 
 class Crawler(object):
+
+    def __init__(self, thread_count, start_url, crawling_depth):
+        self.thread_pool = []
+        self.url_queue = Queue.Queue()
+        self.crawling_depth = crawling_depth
+
+        self.url_queue.put((start_url, 0))
+
+        for i in xrange(thread_count):
+            new_thread = threading.Thread(target=self.thread_action)
+            new_thread.signal = True
+            new_thread.daemon = True
+            self.thread_pool.append(new_thread)
+
+
+    def thread_action(self):
+        while True:
+            try:
+                self.crawl()
+            except Exception:
+                pass
+
+    def start_crawling(self):
+        for thread in self.thread_pool:
+            thread.start()
+
+
+    def crawl(self):
+        current_url, current_depth = self.url_queue.get()
+        print current_url
+        if current_depth == self.crawling_depth:
+            return
+
+        current_page = Crawler.downloadUrl(current_url)
+        links = Crawler.get_all_urls_from_page(current_url, current_page)
+
+        for link in links:
+            self.url_queue.put((link, current_depth + 1))
+
 
     @staticmethod
     def downloadUrl(url):
@@ -36,10 +78,11 @@ class Crawler(object):
 
 def main():
     url = "http://docs.python.org/"
-    page = Crawler.downloadUrl(url)
+    crawler = Crawler(thread_count=1, start_url=url, crawling_depth=2)
+    crawler.start_crawling()
 
-    print Crawler.get_all_urls_from_page(url, page)
-
+    while True:
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
