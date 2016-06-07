7ab0b7106a588da2a6919153f29589e32d6c1fea
Denis Svidunovich <volverinemail@gmail.com>
2016-06-07 21:48:28 +0300

    TSS - is a thread safe python set, with reduced functionality.

---
+++ ISP/4_term/Lab3/Crawler/tss.py
@@ -0,0 +1,35 @@
+import threading
+
+
+class thread_safe_set(object):
+    
+    def __init__(self, obj=None):
+        if obj == None:
+            self.tss = set()
+        else:
+            self.tss = set(obj)
+
+        self.lock = threading.Lock()
+
+    def add(self, obj):
+        try:
+            lock.acquire()
+            tss.append(obj)
+        except Exception, e:
+            print str(e) 
+        finally:
+            lock.release() 
+
+
+    def is_alreay(self, obj):
+        is_already_in_set = True
+
+        try:
+            lock.acquire()
+            is_already_in_set = obj in self.tss
+        except Exception, e:
+            print str(s)
+        finally:
+            lock.release()
+
+        return is_already_in_set
