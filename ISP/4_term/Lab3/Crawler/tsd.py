72d3183bc9dcc9fdb6d3ef53aa3b95fc3af8fb95
Denis Svidunovich <volverinemail@gmail.com>
2016-06-08 16:45:46 +0300

    To json and from json added.

--- ISP/4_term/Lab3/Crawler/tsd.py
+++ ISP/4_term/Lab3/Crawler/tsd.py
@@ -1,6 +1,7 @@
 import threading
+import json
 
-class thread_safe_dictionary(object):
+class thread_safe_dict(object):
     def __init__(self, obj=None):
         if obj == None:
             self.dict = dict()
@@ -13,12 +14,12 @@ class thread_safe_dictionary(object):
 
     def __setitem__(self, key, value):
         try:
-            lock.acquire()
+            self.lock.acquire()
             self.dict[key] = value
         except Exception, e:
             print str(e)    
         finally:
-            lock.release()
+            self.lock.release()
         return
 
 
@@ -26,24 +27,47 @@ class thread_safe_dictionary(object):
         result = False
 
         try:
-            lock.acquire()
+            self.lock.acquire()
             result = key in self.dict
         except Exception, e:
             print str(e)    
         finally:
-            lock.release()
+            self.lock.release()
         return result
 
 
     def __getitem__(self, key):
         result = None
         try:
-            lock.acquire()
+            self.lock.acquire()
             if key in self.dict:
                 result = self.dict[key]
             
         except Exception, e:
             print str(e)    
         finally:
-            lock.release()
+            self.lock.release()
         return result
+
+    def to_json(self):
+        return json.dumps(self.dict, indent=4)
+
+    def from_json(self, text):
+        self.dict = json.loads(text)
+        return
+
+    def write_to_file(self, file_path):
+        with open(file_path, 'w') as f:
+            f.write(self.to_json())
+
+    def read_from_file(self, file_path):
+        text = None
+        
+        if not os.path.exists(file_path):
+            raise Exception('No such file named {}'.format(file_path))
+
+        with open(file_path, 'r') as f:
+            text = f.read()
+
+        self.from_json(text)
+        return
